da599303c345113924292cb9d5bdb7dd69e80c7a
Robert Wilson <youmufan@gmail.com>
2018-03-13 20:48:06 -0400

    initial server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const serverApp = express();
+
+const PORT = process.env.PORT || 5000; // process.env.PORT is necessary for Heroku deployment
+
+serverApp.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+serverApp.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+});
